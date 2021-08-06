ef335d7f52221f5584c7507a8699fefe596a1866
Gustavo Valente <gustavo.pw00@gmail.com>
2021-08-06 10:30:21 -0300

    sniff.py comentado

--- src/sniff.py
+++ src/sniff.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 from scapy.all import *
 
-def process_packet(pkt):
+# Função de configuração, que indica o que fazer com os pacotes sniffados
+def sniffTcpPacket(pkt):
+    # Verifica se existe o TCP no pacote 
     if pkt.haslayer(TCP):
         tcp = pkt[TCP]
+        # Caso exista, apenas imprime a porta da origem e destino e imprime o payload
         print("TCP port: {} --> {} && TCP Payload: {} \n".format(tcp.sport, tcp.dport, tcp.payload))
 
-sniff(iface="lo", filter='tcp', prn=process_packet)
+
+# Função do scapy para fazer o sniffer
+sniff(iface="lo", filter='tcp', prn=sniffTcpPacket)

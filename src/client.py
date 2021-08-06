1e88b017441442ed2fed5d22da446de2d0b324d1
Gustavo Valente <gustavo.pw00@gmail.com>
2021-08-06 16:58:20 -0300

    Log added

--- src/client.py
+++ src/client.py
@@ -1,56 +1,100 @@
 #!/usr/bin/env python3
 import DH
-import sys
 from simplecrypt import encrypt, decrypt
-
 import socket
+import logging as log
+
+format = 'Client - %(levelname)s - %(message)s'
+file = './../log/log.txt'
+log.basicConfig(filename=file, format=format, level=log.INFO)
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+print(f"Server is starting on {HOST} and port {PORT}")
+log.info("Server is starting on %s and port %d", HOST, PORT)
+
 modulo = DH.random_with_N_digits(3)#definindo aleatoriamente o valor que será usado para fazer o módulo
+print(f"Client - Definig the module: {modulo}")
+log.info("Defining the module of the client: %d", modulo)
+
 b = DH.random_with_N_digits(5)#definindo a chave privada
+print(f"Client - Defining the secret key: {b}")
+log.info("Defining the secret key: %d", b)
+
 me = DH.Communicator(5, modulo, b)#criando o objeto comunicador
 partial = me.generatePartialKey()#cria-se a chave parcial
+print(f"Client - Defining the partial key: {partial}")
+log.info("Defining the partial key: %d", partial)
 
-# print("Partial of Client", partial)
+messageType = int(input("Digit the type of communication: \n0: With cryptography\n1: Without cryptography\n"))#0: comunicação criptografada 1: comunicação normal 2: teste
+if(messageType == 0):
+    log.info("Communcation with cryptography")
+else:
+    log.info("Communcation without cryptography")
 
-messageType = int(input("Digit type of communication\n0: with cryptography\n1: without cryptography\n"))#0: comunicação criptografada 1: comunicação normal 2: teste
 
-
+print("Client - Initializing the socket connection")
+log.info("Initializing the socket connection")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     
+    # Send to server the message saying if the communication is with cript or not 
     s.sendall(messageType.to_bytes(5, 'little'))
     data = s.recv(1024)#recebendo ack
     
     if(messageType == 0):
         s.sendall(modulo.to_bytes(5, 'little'))#to_bytes pois o socket envia em bytes object
+        print(f"Client - Sending the module to server: {modulo}")
+        log.info("Sending the module to server")
+
         data = s.recv(1024)#recebendo dados
         partialR = int.from_bytes(data, "little")#chave parcial estrangeira é recebida
+        print(f"Client - Receiving the server partial key: {partialR}")
+        log.info("Receiving the server partial key: %s", partialR)
+
         # print("partial of server ", partialR)
         s.sendall(partial.to_bytes(5, 'little'))#envia a chave parcial do client
+        print(f"Client - Sending the partial key to server: {partial}")
+        log.info("Sending the partial key to server: %s", partial)
         # print("my partial", partial)
         fullKey = me.generateFullKey(partialR)#cria chave inteira
+        print(f"Client - Creating the full key: {fullKey}")
+        log.info("Creating the full key: %s", fullKey)
     
-        inputMessage = input("Type message\n")#pede mensagem ao usuário
+        inputMessage = input("Type a message\n")#pede mensagem ao usuário
+        log.info("Sending a menssagem to server: %s", inputMessage)
+
         message = me.encrypt_message(inputMessage)
+        print(f"Client - Encrypting the message and sending to server: {message}")
+        log.info("Encrypting the message and sending to server: %s", message)
         s.sendall(message)
 
         data = s.recv(1024)
         messageR = me.decrypt_message(data)
+        print(f"Client - Decrypt the answer from the server: {messageR}")
+        log.info("Decrypt the answer from the server: %s", messageR)
+
         print("Answer from server\n->", messageR)
-        print("Testing wrong passwords")
-        for i in range(3):
-            try:
-                messageTest = decrypt(DH.random_with_N_digits(3), data)
-            except:
-                print("Wrong Password!!")
+
+        print("Would you like to test for worng passwords ? ")
+        test = int(input("Y: 1 | N: 0\n"))
+        if test == 1:
+            print("Testing wrong passwords")
+            for i in range(3):
+                try:
+                    messageTest = decrypt(DH.random_with_N_digits(3), data)
+                    print(f"Testing for wrong password: {messageTest}")
+                    log.info("Testing for wrong password: %s", messageTest)
+                except:
+                    print("Wrong Password!!")
 
 
 
     else:
         inputMessage = input("Type message\n")#pede mensagem ao usuário
+        print("Seding a message without criptography")
+        log.info("Sending a message without criptography")
         arr = bytes(inputMessage, 'utf-8')
         s.sendall(arr)
         data = s.recv(1024)
--- src/server.py
+++ src/server.py
@@ -2,48 +2,98 @@
 import DH
 
 import socket
+import logging as log
+
+format = 'Server - %(levelname)s - %(message)s'
+file = './../log/log.txt'
+log.basicConfig(filename=file, format=format, level=log.INFO)
+
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+print(f"Server is starting on {HOST} and port {PORT}")
+log.info("Server is starting on %s and port %d", HOST, PORT)
+
 b = DH.random_with_N_digits(5)
+print("Generating a random number: ", b)
+log.info("Generating a random number: %d", b)
 ack = 1
 
+print("Server - Initializing the socket connection")
+log.info("Initializing the socket connection")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    print("Server - Waiting for a connection")
+    log.info("Waiting for a connection")
+
     conn, addr = s.accept()
     with conn:
-        print('Connected by', addr)
+        print(f"Server - {addr} connected")
+        log.info("%s connected", conn)
         while True:
             data = conn.recv(1024)
+            print("Server - Waiting for client message")
+            log.info("Waiting for client message")
             if not data:
                 break#cliente cancelou conexao
             
+                
             messageType = int.from_bytes(data, 'little')
             conn.sendall(ack.to_bytes(5, 'little'))
+            print("Server - Receiving the message indicanting to server if is to use crypt or not")
+            log.info("Receiving the message indicanting to server if is to use crypt or not")
+
             if(messageType == 0):
                 data = conn.recv(1024)
                 modulo = int.from_bytes(data, "little")#recebe o módulo recebido do cliente e transforma em int
+                print("Server - Receiving the module from client")
+                log.info("Receiving the module from client")
+
                 me = DH.Communicator(5, modulo, b)
                 partial = me.generatePartialKey()
+                print("Server - Generating a partial key: ", partial)
+                log.info("Generating a partial key: %s", partial)
+
                 conn.sendall(partial.to_bytes(5, 'little'))
+                print("Server - Sending the partial key to client")
+                log.info("Sending the partial key to client")
+
                 # print("my partial", partial)
                 data = conn.recv(1024)
                 partialR = int.from_bytes(data, "little")
+                print("Receiving the client partial key: ", partialR)
+                log.info("Receiving the client partial key: %s", partialR)
+
                 # print('Partial Key Received', partialR)
                 data = conn.recv(1024)
-                print('Message Received\n->', data)
-                fullKey = me.generateFullKey(partialR)
-                messageR = me.decrypt_message(data)
-                print("Decrypted\n-> ", messageR)
+                print('Server - Message received', data)
+                log.info("Message received")
 
+                fullKey = me.generateFullKey(partialR)
+                print("Server - Generating full key: ", fullKey)
+                log.info("Generating full key: %s", fullKey)
+
+                print("Decrypting the message: ", fullKey)
+                log.info("Decrypting the message: %s", fullKey)
+
+                messageR = me.decrypt_message(data)
+                print("Server - Message decrypted: ", messageR)
+                log.info("Message decrypted: %s", messageR)
+
+                print("Server - Encrypting the message")
+                log.info("Encrypting the message")
                 message = me.encrypt_message(messageR)
+
+                print("Server - Sending the encrypt message: ", message)
+                log.info("Sending the encrypted message: %s", message)
                 conn.sendall(message)
 
             else:
                 data = conn.recv(1024)
-                print('Message Received\n->', data) 
+                print('Server - Message received', data)
+                log.info("Message received")
                 conn.sendall(data)
 
 
